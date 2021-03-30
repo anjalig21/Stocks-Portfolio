a1746b8aef3ddec69873138e6ecdc98faaf30850
anjalig21 <anjaligta@outlook.com>
2021-03-29 22:50:23 -0400

    fixed api bugs

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Welcome")
+})
+
 app.get("/:ticker", (req, res) => {
     async function sendResponse() {
         const url = `https://finnhub.io/api/v1/quote?symbol=${req.params.ticker}&token=c1h8gb748v6t9ghtphbg`;
@@ -11,9 +15,13 @@ app.get("/:ticker", (req, res) => {
                 return res;
             })
             .catch((err) => {
-                console.log(err);
+                return err;
             })
-            res.send(result);
+            const sendData = {
+                "ticker": req.params.ticker,
+                "price": result.data.c
+            }
+            res.send(sendData);
     }
     sendResponse();
 })
