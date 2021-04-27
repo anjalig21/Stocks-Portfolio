0c7648354900f7e2e21a08f9020d83411bdc6a72
anjalig21 <anjaligta@outlook.com>
2021-04-27 12:08:43 -0400

    added UI

--- express/app.js
+++ express/app.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const axios = require('axios');
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 const fs = require('fs');
 const { Pool } = require("pg");
+const cors = require('cors')
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
@@ -27,44 +29,38 @@ const config = {
 };
 
 // Create a connection pool
-//let client;
 const pool = new Pool(config);
-// (async () => {
-//     client = await pool.connect();
-// })().catch((err) => console.log(err.stack));
 
-// (async () => {
-//     // Connect to database
-//     const client = await pool.connect();
-//     await client.query(
-//         "CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY,  VARCHAR(200), );",
-//         (err, res) => {
-//             if (err) throw err;
-//         }
-// })().catch((err) => console.log(err.stack));
+async function getPrice(ticker) {
+    const url = `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${process.env.TOKEN}`;
+    const result = await axios.get(url)
+        .then((res) => {
+            return res;
+        })
+    return result;
+}
 
+// Gets price of stock ticker from Finnhub API
 app.get("/", (req, res) => {
     if (!req.query.ticker) {
         res.send("Welcome")
     }
     async function sendResponse() {
-        const url = `https://finnhub.io/api/v1/quote?symbol=${req.query.ticker}&token=c1h8gb748v6t9ghtphbg`;
-        const result = await axios.get(url)
-            .then((res) => {
-                return res;
-            })
-            .catch((err) => {
-                res.sendStatus('400').end();
-            })
-        const sendData = {
-            "ticker": req.params.ticker,
-            "price": result.data.c
+        const result = await getPrice(req.query.ticker);
+        if (result.data.c === 0) {
+            res.status(400).send('Ticker does not exist');
+        } else {
+            const sendData = {
+                "ticker": req.query.ticker,
+                "price": result.data.c
+            }
+            res.status(200).send(sendData);
         }
-        res.send(sendData);
     }
     sendResponse();
 })
 
+// Clears database
 app.delete("/", (req, res) => {
     async function deleteDb() {
         try {
@@ -72,7 +68,7 @@ app.delete("/", (req, res) => {
             await client.query(
                 "SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE';",
                 async (err, table) => {
-                    console.log(table.rows);
+                    // console.log(table.rows);
                     if (err) throw err;
                     let length = table.rowCount;
                     for (let i = 0; i < length; i++) {
@@ -93,6 +89,38 @@ app.delete("/", (req, res) => {
     deleteDb();
 })
 
+app.delete("/portfolio/:account", (req, res) => {
+    async function deleteTicker() {
+        try {
+            const client = await pool.connect();
+            await client.query(
+                `IF EXISTS (SELECT * from ${account})`,
+                async (err, table) => {
+                    // console.log(table.rows);
+                    if (err) throw err;
+                    let length = table.rowCount;
+                    for (let i = 0; i < length; i++) {
+                        const found = req.body.find((t, idx) => t.tickers === table.rows[i].tickers);
+                        if (found) {
+                            await client.query(
+                                `DELETE FROM ${account} WHERE tickers=${table.rows[i].tickers};`,
+                                (err, response) => {
+                                    if (err) throw err;
+                                }
+                            )
+                        }
+                    }
+                    console.log("Tickers Deleted");
+                })
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+    deleteTicker();
+})
+
+// Creates and Updates User's Portfolio (adds more ticker stocks)
 app.post("/portfolio/:account", (req, res) => {
     async function sendResponse() {
         var result;
@@ -153,6 +181,7 @@ app.post("/portfolio/:account", (req, res) => {
     sendResponse();
 })
 
+// Gets the stock tickers in user's portfolio and returns price of each ticker by using the Finhub API
 app.get("/portfolio/:account", (req, res) => {
     async function getPrice() {
         try {
@@ -161,7 +190,6 @@ app.get("/portfolio/:account", (req, res) => {
             await client.query(
                 `SELECT EXISTS(SELECT * FROM ${req.params.account})`,
                 async (error, response) => {
-                    console.log(response);
                     if (error) {
                         res.status('400').send('User does not exist');
                         throw error;
@@ -169,14 +197,10 @@ app.get("/portfolio/:account", (req, res) => {
                     let length = response.rowCount;
                     for (let i = 0; i < length; i++) {
                         let ticker = response.rows[i].tickers;
-                        const url = `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=c1h8gb748v6t9ghtphbg`;
-                        const result = await axios.get(url)
-                            .then((res) => {
-                                return res;
-                            })
-                            .catch((err) => {
-                                res.status('400').send('Ticker does not exist');
-                            })
+                        const result = await getPrice(ticker);
+                        if (result.data.c === 0) {
+                            res.status(400).send('Ticker does not exist');
+                        }
                         priceArray.push({ "ticker": ticker, "price": result.data.c });
                     }
                     res.send(priceArray);
